f38c08a018fcb39a578b3e0df68f653b0ee03fcc
alexkingorg <alexkingorg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2006-12-12 07:34:24 +0000

    version 1.3
    git-svn-id: https://plugins.svn.wordpress.org/share-this/trunk@7067 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- share-this/share-this.php
+++ share-this/share-this.php
@@ -24,11 +24,11 @@ Author URI: http://alexking.org/
 */
 
 
-@define('AKST_ADDTOCONTENT', true);
+@define('AKST_ADDTOCONTENT', false);
 // set this to false if you do not want to automatically add the Share This link to your content
 
 
-@define('AKST_ADDTOFOOTER', true);
+@define('AKST_ADDTOFOOTER', false);
 // set this to false if you do not want to automatically add the Share This form to the page in your footer
 
 
@@ -106,7 +106,7 @@ $social_sites = array(
 // NO NEED TO EDIT BELOW THIS LINE
 // ============================================================
 
-@define('AK_WPROOT', '../../../');
+@define('AK_WPROOT', '/www/v2.alexking.org/wp/');
 @define('AKST_FILEPATH', '/wp-content/plugins/share-this/share-this.php');
 
 if (function_exists('load_plugin_textdomain')) {
