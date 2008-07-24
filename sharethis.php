d6343d8b2fea534602f9125f4b3f4fbeab6f7626
alexkingorg <alexkingorg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-24 18:04:13 +0000

    adding 2.2
    git-svn-id: https://plugins.svn.wordpress.org/share-this/tags/2.2@56143 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sharethis.php
+++ sharethis.php
@@ -24,7 +24,7 @@
 Plugin Name: ShareThis
 Plugin URI: http://sharethis.com
 Description: Let your visitors share a post/page with others. Supports e-mail and posting to social bookmarking sites. <a href="options-general.php?page=sharethis.php">Configuration options are here</a>. Questions on configuration, etc.? Make sure to read the README.
-Version: 2.1
+Version: 2.2
 Author: ShareThis and Crowd Favorite (crowdfavorite.com)
 Author URI: http://sharethis.com
 */
@@ -174,10 +174,8 @@ function st_remove_st_add_link($content) {
 	return $content;
 }
 
-// 2006-06-02 cleaned up the logic here, renamed function, added is_feed() check in st_add_feed_link() also
-// 2006-06-02 is_page() was returning TRUE always, so using is_single() to differentiate the two types of content	
 function st_add_widget($content) {
-	if ((is_single() && get_option('st_add_to_page') != 'no') || (!is_single() && get_option('st_add_to_content') != 'no')) {
+	if ((is_page() && get_option('st_add_to_page') != 'no') || (!is_page() && get_option('st_add_to_content') != 'no')) {
 		if (!is_feed()) {
 			return $content.'<p>'.st_widget().'</p>';
 		}
