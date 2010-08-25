e19a884afe9e3c39773927b427f5b5f175ed6a1b
ShareThis <ShareThis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-25 19:17:26 +0000

    Fixed Readme File
    git-svn-id: https://plugins.svn.wordpress.org/share-this/trunk@280728 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sharethis.php
+++ sharethis.php
@@ -22,7 +22,7 @@
  Plugin Name: ShareThis
  Plugin URI: http://sharethis.com
  Description: Let your visitors share a post/page with others. Supports e-mail and posting to social bookmarking sites. <a href="options-general.php?page=sharethis.php">Configuration options are here</a>. Questions on configuration, etc.? Make sure to read the README.
- Version: 4.0
+ Version: 4.0.1
  Author: ShareThis, Manu Mukerji <manu@sharethis.com>
  Author URI: http://sharethis.com
  */
