dbf28d8332532760ee43044189b5ac1617b14721
ShareThis <ShareThis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-17 18:53:57 +0000

    push for 5.0.1
    git-svn-id: https://plugins.svn.wordpress.org/share-this/tags/5.0.1@559475 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_st_opt.js
+++ wp_st_opt.js
@@ -8,6 +8,9 @@ if (!window.console || !console.firebug) {
 
 var startPos=1;
 
+// Do not make tags on page load. call Make Tags once the user changes any settings.
+var makeTagsEnabled = false;
+
 function st_log() {
 	_gaq.push(['_trackEvent', 'WordPressPlugin', 'ConfigOptionsUpdated']);
 	_gaq.push(['_trackEvent', 'WordPressPlugin', "Type_" + $("#st_current_type").val()]);
@@ -43,7 +46,7 @@ jQuery(document).ready(function() {
 		scroll:1,
 		visible:1,
 		start:startPos,
-		wrap:"circular",
+		wrap:"both",
 		itemFirstInCallback: {
 		  onAfterAnimation: carDoneCB
 		},
@@ -60,6 +63,92 @@ jQuery(document).ready(function() {
 		stpkeytimeout=setTimeout(function(){makeHeadTag();},500);
 	})
 
+	var services=$('#st_services').val();
+	svc=services.split(",");
+	for(var i=0;i<svc.length;i++){
+		if (svc[i]=="fblike"){
+			$('#st_fblike').attr('checked','checked');
+		} else if (svc[i]=="plusone"){
+			$('#st_plusone').attr('checked','checked');
+		} else if (svc[i]=="pinterest"){
+			$('#st_pinterest').attr('checked','checked');
+		}
+	}
+	
+	$('#st_fblike').bind('click', function(){
+		if ($('#st_fblike').attr('checked')) {
+			if ($('#st_services').val().indexOf("fblike")==-1) {
+				var pos=$('#st_services').val().indexOf("plusone");
+				if (pos==-1)
+					$('#st_services').val($('#st_services').val()+",fblike");
+				else {
+					var str=$('#st_services').val();
+					if (pos==0)
+						$('#st_services').val("fblike,"+str.substr(pos));
+					else
+						$('#st_services').val(str.substr(0,pos-1)+",fblike"+str.substr(pos-1));
+				}
+			}
+		}
+		else {
+			var pos=$('#st_services').val().indexOf("fblike");
+			if (pos!=-1) {
+				var str=$('#st_services').val();
+				if (pos==0)
+					$('#st_services').val(str.substr(pos+7));
+				else
+					$('#st_services').val(str.substr(0,pos-1)+str.substr(pos+6));
+			}
+		}
+		clearTimeout(stpkeytimeout);
+		stpkeytimeout=setTimeout(function(){makeTags();},500);
+	})
+	
+	$('#st_plusone').bind('click', function(){
+		if ($('#st_plusone').attr('checked')) {
+			if ($('#st_services').val().indexOf("plusone")==-1) {
+				$('#st_services').val($('#st_services').val()+",plusone");
+			}
+		}
+		else {
+			var pos=$('#st_services').val().indexOf("plusone");
+			if (pos!=-1) {
+				var str=$('#st_services').val();
+				if (pos==0)
+					$('#st_services').val(str.substr(pos+8));
+				else
+					$('#st_services').val(str.substr(0,pos-1)+str.substr(pos+7));
+			}
+		}
+		clearTimeout(stpkeytimeout);
+		stpkeytimeout=setTimeout(function(){makeTags();},500);
+	})
+	
+	$('#st_pinterest').bind('click', function(){
+		if ($('#st_pinterest').attr('checked')) {
+			if ($('#st_services').val().indexOf("pinterest")==-1) {
+				$('#st_services').val($('#st_services').val()+",pinterest");
+			}
+		}
+		else {
+			var pos=$('#st_services').val().indexOf("pinterest");
+			if (pos!=-1) {
+				var str=$('#st_services').val();
+				if (pos==0)
+					$('#st_services').val(str.substr(pos+10));
+				else
+					$('#st_services').val(str.substr(0,pos-1)+str.substr(pos+9));
+			}
+		}
+		clearTimeout(stpkeytimeout);
+		stpkeytimeout=setTimeout(function(){makeTags();},500);
+	})
+	
+	
+	$(".registerLink").live('click',function() {
+		createOverlay();
+	});
+	
 });
 
 var stkeytimeout=null;
@@ -87,12 +176,15 @@ function makeTags(){
 		$('#st_tags').val(tags);
 		return true;
 	}
-	if(type=="chicklet" || type=="chicklet2" || type=="classic"){
+	if(type=="chicklet" || type=="classic"){
 		type="";
 	}
 	for(var i=0;i<svc.length;i++){
 		if(svc[i].length>2){
-			tags+="<span class='st_"+svc[i]+type+"' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' "+dt+"></span>";
+			if(type =="chicklet2")
+				tags+="<span class='st_"+svc[i]+"' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>";
+			else
+				tags+="<span class='st_"+svc[i]+type+"' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' displayText='"+svc[i]+"'></span>";
 		}
 	}
 	$('#st_tags').val(tags);
@@ -103,6 +195,7 @@ function makeTags(){
 function carDoneCB(a,elem){
 	var type=elem.getAttribute("st_type");
 	$('.services').show()
+	$('.fblikeplusone').show();
 	if(type=="vcount"){
 		$('#curr_type').html("_vcount");$("#st_current_type").val("_vcount");
 		$('#currentType').html("<span class='type_name'>Vertical Count</span>");
@@ -123,10 +216,14 @@ function carDoneCB(a,elem){
 			$('#currentType').html("<span class='type_name'>Regular Buttons No-Text</span>");
 	}else if(type=="sharethis"){
 			$('.services').hide();
+			$('.fblikeplusone').hide();
 			$('#curr_type').html("classic");$("#st_current_type").val("classic");
 			$('#currentType').html("<span class='type_name'>Classic</span>");
 	}	
+	if(makeTagsEnabled == true) {
 	makeTags();	
+}
+	makeTagsEnabled = true;
 }
 
 $(".versionItem").click(function() {
@@ -134,4 +231,20 @@ $(".versionItem").click(function() {
 	$(this).addClass("versionSelect");	
 });
 
+var container = null;
+function createOverlay () {
+		container = $('<div id="registratorCodeModal" class="registratorCodeModal"></div><div class="registratorModalWindowContainer"><div id="registratorModalWindow"></div></div>');
+		$("body").append(container);
 
+		var div = container.find("#registratorModalWindow");
+		var html = "<div class='registratorContainer'>";
+		html += "<div onclick=javascript:container.remove(); class='registratorCloser'></div>";
+		html += "<iframe height='341px' width='551px' src='http://sharethis.com/external-login' />";
+		div.append(html);
+}
+
+$(document).keydown(function(e) {
+		if (e.keyCode == 27 && container!=null) { 
+			container.remove(); 
+		}
+});
