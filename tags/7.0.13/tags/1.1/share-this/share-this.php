336e79a67d5c585584da8323aaa3af131406b814
ShareThis <ShareThis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-30 04:32:58 +0000

    Tagging the 7.0.13 release of the project
    git-svn-id: https://plugins.svn.wordpress.org/share-this@905270 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/7.0.13/tags/1.1/share-this/share-this.php
@@ -0,0 +1,495 @@
+<?php
+
+// Share This
+//
+// Copyright (c) 2006 Alex King
+// http://alexking.org/projects/wordpress
+//
+// This is an add-on for WordPress
+// http://wordpress.org/
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// *****************************************************************
+
+/*
+Plugin Name: Share This
+Plugin URI: http://alexking.org/projects/wordpress
+Description: Let your visitors share a post/page with others. Supports e-mail and posting to social bookmarking sites. Thanks to <a href="http://www.twistermc.com/">Thomas McMahon</a> for footwork on the URLs.
+Version: 1.1
+Author: Alex King
+Author URI: http://alexking.org/
+*/
+
+if (function_exists('load_plugin_textdomain')) {
+	load_plugin_textdomain('alexking.org');
+}
+
+// Find more URLs here: 
+// http://3spots.blogspot.com/2006/02/30-social-bookmarks-add-to-footer.html
+
+$social_sites = array(
+	'delicious' => array(
+		'name' => 'del.icio.us'
+		, 'url' => 'http://del.icio.us/post?url={url}&amp;title={title}'
+	)
+	, 'digg' => array(
+		'name' => 'Digg'
+		, 'url' => 'http://digg.com/submit?phase=2&amp;url={url}'
+	)
+	, 'furl' => array(
+		'name' => 'Furl'
+		, 'url' => 'http://furl.net/storeIt.jsp?u={url}&amp;t={title}'
+	)
+	, 'netscape' => array(
+		'name' => 'Netscape'
+		, 'url' => ' http://www.netscape.com/submit/?U={url}&amp;T={title}'
+	)
+	, 'yahoo_myweb' => array(
+		'name' => 'Yahoo! My Web'
+		, 'url' => 'http://myweb2.search.yahoo.com/myresults/bookmarklet?u={url}&amp;t={title}'
+	)
+	, 'technorati' => array(
+		'name' => 'Technorati'
+		, 'url' => 'http://www.technorati.com/faves?add={url}'
+	)
+	, 'google_bmarks' => array(
+		'name' => 'Google Bookmarks'
+		, 'url' => '  http://www.google.com/bookmarks/mark?op=edit&amp;bkmk={url}&amp;title={title}'
+	)
+	, 'newsvine' => array(
+		'name' => 'Newsvine'
+		, 'url' => 'http://www.newsvine.com/_wine/save?u={url}&amp;h={title}'
+	)
+	, 'blinklist' => array(
+		'name' => 'BlinkList'
+		, 'url' => 'http://blinklist.com/index.php?Action=Blink/addblink.php&amp;Url={url}&amp;Title={title}'
+	)
+	, 'reddit' => array(
+		'name' => 'reddit'
+		, 'url' => 'http://reddit.com/submit?url={url}&amp;title={title}'
+	)
+	, 'blogmarks' => array(
+		'name' => 'Blogmarks'
+		, 'url' => 'http://blogmarks.net/my/new.php?mini=1&amp;url={url}&amp;title={title}'
+	)
+	, 'magnolia' => array(
+		'name' => 'ma.gnolia'
+		, 'url' => 'http://ma.gnolia.com/bookmarklet/add?url={url}&amp;title={title}'
+	)
+	, 'windows_live' => array(
+		'name' => 'Windows Live'
+		, 'url' => 'https://favorites.live.com/quickadd.aspx?marklet=1&amp;mkt=en-us&amp;url={url}&amp;title={title}&amp;top=1'
+	)
+	, 'tailrank' => array(
+		'name' => 'Tailrank'
+		, 'url' => 'http://tailrank.com/share/?link_href={url}&amp;title={title}'
+	)
+);
+
+// set this to false if you do not want to automatically add the Share This link to your content
+
+@define('AKST_ADDTOCONTENT', true);
+@define('AKST_ADDTOFOOTER', true);
+
+@define('AK_WPROOT', '../../../');
+@define('AKST_FILEPATH', '/wp-content/plugins/share-this/share-this.php');
+
+if (!function_exists('ak_check_email_address')) {
+	function ak_check_email_address($email) {
+// From: http://www.ilovejackdaniels.com/php/email-address-validation/
+// First, we check that there's one @ symbol, and that the lengths are right
+		if (!ereg("^[^@]{1,64}@[^@]{1,255}$", $email)) {
+			// Email invalid because wrong number of characters in one section, or wrong number of @ symbols.
+			return false;
+		}
+// Split it into sections to make life easier
+		$email_array = explode("@", $email);
+		$local_array = explode(".", $email_array[0]);
+		for ($i = 0; $i < sizeof($local_array); $i++) {
+			 if (!ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $local_array[$i])) {
+				return false;
+			}
+		}	
+		if (!ereg("^\[?[0-9\.]+\]?$", $email_array[1])) { // Check if domain is IP. If not, it should be valid domain name
+			$domain_array = explode(".", $email_array[1]);
+			if (sizeof($domain_array) < 2) {
+					return false; // Not enough parts to domain
+			}
+			for ($i = 0; $i < sizeof($domain_array); $i++) {
+				if (!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i])) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
+
+if (!empty($_REQUEST['akst_action'])) {
+	switch ($_REQUEST['akst_action']) {
+		case 'js':
+			header("Content-type: text/javascript");
+?>
+function akst_share(id, url, title) {
+	var form = $('akst_form');
+	var link = $('akst_link_' + id);
+	var offset = Position.cumulativeOffset(link);
+
+<?php
+	foreach ($social_sites as $key => $data) {
+		print('	$("akst_'.$key.'").href = akst_share_url("'.$data['url'].'", url, title);'."\n");
+	}
+?>
+
+	$('akst_post_id').value = id;
+
+	form.style.left = offset[0] + 'px';
+	form.style.top = (offset[1] + link.offsetHeight + 3) + 'px';
+	form.style.display = 'block';
+}
+
+function akst_share_url(base, url, title) {
+	base = base.replace('{url}', url);
+	return base.replace('{title}', title);
+}
+
+function akst_share_tab(tab) {
+	var tab1 = document.getElementById('akst_tab1');
+	var tab2 = document.getElementById('akst_tab2');
+	var body1 = document.getElementById('akst_social');
+	var body2 = document.getElementById('akst_email');
+	
+	switch (tab) {
+		case '1':
+			tab2.className = '';
+			tab1.className = 'selected';
+			body2.style.display = 'none';
+			body1.style.display = 'block';
+			break;
+		case '2':
+			tab1.className = '';
+			tab2.className = 'selected';
+			body1.style.display = 'none';
+			body2.style.display = 'block';
+			break;
+	}
+}
+
+function akst_xy(id) {
+	var element = $(id);
+	var x = 0;
+	var y = 0;
+}
+<?php
+			die();
+			break;
+		case 'css':
+			header("Content-type: text/css");
+?>
+#akst_form {
+	background: #999;
+	border: 1px solid #ddd;
+	display: none;
+	position: absolute;
+	width: 350px;
+}
+#akst_form a.akst_close {
+	color: #fff;
+	float: right;
+	margin: 5px;
+}
+#akst_form ul.tabs {
+	border: 1px solid #999;
+	list-style: none;
+	margin: 10px 10px 0 10px;
+	padding: 0;
+}
+#akst_form ul.tabs li {
+	background: #ccc;
+	border-bottom: 1px solid #999;
+	cursor: pointer;
+	float: left;
+	margin: 0 3px 0 0;
+	padding: 3px 5px 2px 5px;
+}
+#akst_form ul.tabs li.selected {
+	background: #fff;
+	border-bottom: 1px solid #fff;
+	cursor: default;
+	padding: 4px 5px 1px 5px;
+}
+#akst_form div.clear {
+	clear: both;
+	float: none;
+}
+#akst_social, #akst_email {
+	background: #fff;
+	border: 1px solid #fff;
+	padding: 10px;
+}
+#akst_social ul {
+	list-style: none;
+	margin: 0;
+	padding: 0;
+}
+#akst_social ul li {
+	float: left;
+	margin: 0;
+	padding: 0;
+	width: 45%;
+}
+#akst_social ul li a {
+	display: block;
+	float: left;
+	height: 24px;
+	padding: 4px 0 0 24px;
+	vertical-align: middle;
+}
+<?php
+foreach ($social_sites as $key => $data) {
+	print(
+'#akst_'.$key.' {
+	background: url('.$key.'.gif) no-repeat 2px center;
+}
+');
+}
+?>
+#akst_email {
+	display: none;
+	text-align: left;
+}
+#akst_email form, #akst_email fieldset {
+	border: 0;
+	margin: 0;
+	padding: 0;
+}
+#akst_email fieldset legend {
+	display: none;
+}
+#akst_email ul {
+	list-style: none;
+	margin: 0;
+	padding: 0;
+}
+#akst_email ul li {
+	margin: 0 0 7px 0;
+	padding: 0;
+}
+#akst_email ul li label {
+	color: #555;
+	display: block;
+	margin-bottom: 3px;
+}
+#akst_email ul li input {
+	padding: 3px 10px;
+}
+#akst_email ul li input.akst_text {
+	padding: 3px;
+	width: 280px;
+}
+
+<?php
+			die();
+			break;
+		case 'send_mail':
+			require(AK_WPROOT.'wp-blog-header.php');
+
+			if (function_exists('load_plugin_textdomain')) {
+				load_plugin_textdomain('alexking.org');
+			}
+
+			$post_id = '';
+			$to = '';
+			$name = '';
+			$email = '';
+			
+			if (!empty($_REQUEST['akst_to'])) {
+				$to = stripslashes($_REQUEST['akst_to']);
+				$to = str_replace(
+					array(
+						','
+						,"\n"
+						,"\t"
+						,"\r"
+					)
+					, array()
+					, $to
+				);
+			}
+			
+			if (!empty($_REQUEST['akst_name'])) {
+				$name = stripslashes($_REQUEST['akst_name']);
+				$name = str_replace(
+					array(
+						'"'
+						,"\n"
+						,"\t"
+						,"\r"
+					)
+					, array()
+					, $name
+				);
+			}
+
+			if (!empty($_REQUEST['akst_email'])) {
+				$email = stripslashes($_REQUEST['akst_email']);
+				$email = str_replace(
+					array(
+						','
+						,"\n"
+						,"\t"
+						,"\r"
+					)
+					, array()
+					, $email
+				);
+			}
+			
+			if (!empty($_REQUEST['akst_post_id'])) {
+				$post_id = intval($_REQUEST['akst_post_id']);
+			}
+
+			if (empty($post_id) || empty($to) || !ak_check_email_address($to) || empty($email) || !ak_check_email_address($email)) {
+				wp_die(__('Click your <strong>back button</strong> and make sure those e-mail addresses are valid then try again.', 'alexking.org'));
+			}
+			
+			$post = &get_post($post_id);
+			
+			$url = get_permalink($post_id);
+			
+			$headers = "MIME-Version: 1.0\n" .
+				'From: "'.$name.'" <'.$email.'>'."\n"
+				.'Reply-To: "'.$name.'" <'.$email.'>'."\n"
+				.'Return-Path: "'.$name.'" <'.$email.'>'."\n"
+				."Content-Type: text/plain; charset=\"" . get_option('blog_charset') ."\"\n";
+			
+			$subject = __('Check out this post on ', 'alexking.org').get_bloginfo('name');
+			
+			$message = __('Greetings--', 'alexking.org')."\n\n"
+				.$name.__(' thinks this will be of interest to you:', 'alexking.org')."\n\n"
+				.$url."\n\n"
+				.__('Enjoy.', 'alexking.org')."\n\n"
+				.'--'."\n"
+				.get_bloginfo('home')."\n";
+			
+			@mail($to, $subject, $message, $headers);
+			
+			if (!empty($_SERVER['HTTP_REFERER'])) {
+				$url = $_SERVER['HTTP_REFERER'];
+			}
+			
+			header("Location: $url");
+			status_header('302');
+			die();
+			
+			break;
+	}
+}
+
+function akst_head() {
+	$wp = get_bloginfo('wpurl');
+	$url = $wp.AKST_FILEPATH;
+	print('
+	<script type="text/javascript" src="'.$wp.'/wp-includes/js/prototype.js"></script>
+	<script type="text/javascript" src="'.$url.'?akst_action=js"></script>
+	<link rel="stylesheet" type="text/css" href="'.$url.'?akst_action=css" />
+	');
+}
+add_action('wp_head', 'akst_head');
+
+function akst_share_link($action = 'print') {
+	global $post;
+	ob_start();
+?>
+<a href="javascript:void(akst_share('<?php print($post->ID); ?>', '<?php print(urlencode(get_permalink($post->ID))); ?>', '<?php print(urlencode(get_the_title())); ?>'));" title="<?php _e('E-mail this, post to del.icio.us, etc.', 'alexking.org'); ?>" id="akst_link_<?php print($post->ID); ?>"><?php _e('Share This', 'alexking.org'); ?></a>
+<?php
+	$link = ob_get_contents();
+	ob_end_clean();
+	switch ($action) {
+		case 'print':
+			print($link);
+			break;
+		case 'return':
+			return $link;
+			break;
+	}
+}
+
+function akst_add_share_link_to_content($content) {
+	$content .= '<p class="akst_link">'.akst_share_link('return').'</p>';
+	return $content;
+}
+if (AKST_ADDTOCONTENT) {
+	add_action('the_content', 'akst_add_share_link_to_content');
+}
+
+function akst_share_form() {
+	global $post, $social_sites, $current_user;
+
+	if (isset($current_user)) {
+		$user = get_currentuserinfo();
+		$name = $current_user->user_nicename;
+		$email = $current_user->user_email;
+	}
+	else {
+		$user = wp_get_current_commenter();
+		$name = $user['comment_author'];
+		$email = $user['comment_author_email'];
+	}
+?>
+	<!-- Share This BEGIN -->
+	<div id="akst_form">
+		<a href="javascript:void($('akst_form').style.display='none');" class="akst_close"><?php _e('Close', 'alexking.org'); ?></a>
+		<ul class="tabs">
+			<li id="akst_tab1" class="selected" onclick="akst_share_tab('1');"><?php _e('Social Web', 'alexking.org'); ?></li>
+			<li id="akst_tab2" onclick="akst_share_tab('2');"><?php _e('E-mail', 'alexking.org'); ?></li>
+		</ul>
+		<div class="clear"></div>
+		<div id="akst_social">
+			<ul>
+<?php
+	foreach ($social_sites as $key => $data) {
+		print('				<li><a href="#" id="akst_'.$key.'">'.$data['name'].'</a></li>'."\n");
+	}
+?>
+			</ul>
+			<div class="clear"></div>
+		</div>
+		<div id="akst_email">
+			<form action="<?php print(get_bloginfo('wpurl').AKST_FILEPATH); ?>" method="post">
+				<fieldset>
+					<legend><?php _e('E-mail It', 'alexking.org'); ?></legend>
+					<ul>
+						<li>
+							<label><?php _e('To Address:', 'alexking.org'); ?></label>
+							<input type="text" name="akst_to" value="" class="akst_text" />
+						</li>
+						<li>
+							<label><?php _e('Your Name:', 'alexking.org'); ?></label>
+							<input type="text" name="akst_name" value="<?php print(htmlspecialchars($name)); ?>" class="akst_text" />
+						</li>
+						<li>
+							<label><?php _e('Your Address:', 'alexking.org'); ?></label>
+							<input type="text" name="akst_email" value="<?php print(htmlspecialchars($email)); ?>" class="akst_text" />
+						</li>
+						<li>
+							<input type="submit" name="akst_submit" value="<?php _e('Send It', 'alexking.org'); ?>" />
+						</li>
+					</ul>
+					<input type="hidden" name="akst_action" value="send_mail" />
+					<input type="hidden" name="akst_post_id" id="akst_post_id" value="" />
+				</fieldset>
+			</form>
+		</div>
+	</div>
+	<!-- Share This END -->
+<?php
+}
+if (AKST_ADDTOFOOTER) {
+	add_action('wp_footer', 'akst_share_form');
+}
+
+?>
---
+++ tags/7.0.13/tags/1.3/README.txt
@@ -0,0 +1,94 @@
+=== Share This ===
+Tags: email, e-mail, bookmark, social, network, digg, del.icio.us, ma.gnolia, technorati, reddit, tailrank, furl, blinklist, blogmarks, newsvine, netscape, social, socialize
+Contributors: alexkingorg
+Requires at least: 1.5
+Tested up to: 2.0.5
+Stable tag: 1.3
+
+Share This is a plugin that provides an unobtrusive way for your visitors to add your post to various social bookmarking sites, or send a link via e-mail to a friend.
+
+== Installation ==
+
+1. Download the plugin archive and expand it (you've likely already done this).
+2. Put the 'share-this' folder into your wp-content/plugins/ directory.
+3. Go to the Plugins page in your WordPress Administration area and click 'Activate' for Share This.
+4. If you are using a version of WP prior to 2.1, upload the included prototype.js to your wp-includes/js/ directory
+
+== Template Tags ==
+
+By default, the Share This link will be added to the end of your content and the Share This form will be added to your footer. If you would like to control exactly where these go in your template, you can do so by disabling the auto-output and using the following template tags:
+
+The link:
+
+`<?php akst_share_link(); ?>`
+
+The form:
+
+`<?php akst_share_form(); ?>`
+
+To disable the auto-output of the link and form, you can edit the .php file and change the following settings from:
+
+`@define('AKST_ADDTOCONTENT', true);`
+`@define('AKST_ADDTOFOOTER', true);`
+
+to:
+
+`@define('AKST_ADDTOCONTENT', false);`
+`@define('AKST_ADDTOFOOTER', false);`
+
+If you don't want to edit the .php file, you can add this to your WordPress index.php file or your own plugin (which must be loaded before Share This):
+
+`@define('AKST_ADDTOCONTENT', false);`
+`@define('AKST_ADDTOFOOTER', false);`
+
+== Known Issues ==
+
+If your theme does not include a wp_footer() call, you will need to add the akst_share_form template tag to your footer manually:
+
+`<?php akst_share_form(); ?>`
+
+or add the `<?php wp_footer(); ?>` tag to your theme footer.
+
+== Frequently Asked Questions ==
+
+= How do I add or remove social sites? =
+
+You can add or remove social sites by editing the share-this.php file. Use {url} and {title} in the URL to represent your post URL and title as you see done in the examples.
+
+= How do I hide the icon? =
+
+Change the following in the plugin file:
+
+`@define('AKST_SHOWICON', true);`
+
+to:
+
+`@define('AKST_SHOWICON', false);`
+
+or define your own CSS class information for `.akst_share_link`.
+
+= What happens if a user has JavaScript disabled or is using a mobile devide? =
+
+Realistically, if a user has JavaScript disabled or is using a mobile device, it's unlikely they will want to sharing an item via social web sites, e-mail, etc. However, as of version 1.3 this is now supported and they will be taken to a custom page that allows this.
+
+= Does Share This work in feeds too? =
+
+Yes, as of version 1.3.
+
+= How do I remove the link from items in my feeds? =
+
+Change the following in the plugin file:
+
+`@define('AKST_ADDTOFEED', true);`
+
+to:
+
+`@define('AKST_ADDTOFEED', true);`
+
+= Anything else? =
+
+That about does it - enjoy!
+
+--Alex King
+
+http://alexking.org/projects/wordpress
